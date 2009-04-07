7a61f1b94de084245259088d2b646d8243c65f2f
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-04-07 15:00:57 +0000

    * add a test but not passed.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/activegroonga/trunk@36 2d713de4-4fbb-4d10-8406-bb89ef438322

--- test/active-groonga-test-utils.rb
+++ test/active-groonga-test-utils.rb
@@ -22,9 +22,19 @@ require 'active_groonga'
 module ActiveGroongaTestUtils
   class << self
     def included(base)
-      base.setup :setup_tmp_directory, :before => :prepend
-      base.teardown :teardown_tmp_directory, :after => :append
+      base.setup :setup_sand_box, :before => :prepend
+      base.teardown :teardown_sand_box, :after => :append
     end
+  end
+
+  def setup_sand_box
+    Groonga::Context.default = nil
+    @context = Groonga::Context.default
+    setup_tmp_directory
+    setup_database
+    setup_table
+    setup_records
+    setup_class
   end
 
   def setup_tmp_directory
@@ -33,7 +43,52 @@ module ActiveGroongaTestUtils
     FileUtils.mkdir_p(@tmp_dir.to_s)
   end
 
+  def setup_database
+    @db_path = @tmp_dir + "db"
+    @database = Groonga::Database.create(:path => @db_path.to_s)
+  end
+
+  def setup_table
+    @tables_dir = @tmp_dir + "tables"
+    @tables_dir.mkdir
+
+    @bookmarks_path = @tables_dir + "bookmarks"
+    @bookmarks = Groonga::Array.create(:name => "bookmarks",
+                                       :path => @bookmarks_path.to_s)
+
+    @uri_column = @bookmarks.define_column("uri", "<shorttext>")
+    @comment_column = @bookmarks.define_column("comment", "<text>")
+#     @content_column = @bookmarks.define_column("content", "<longtext>",
+#                                                :type => "index",
+#                                                :with_section => true,
+#                                                :with_weight => true,
+#                                                :with_position => true)
+  end
+
+  def setup_records
+    @bookmark_records = {}
+
+    @bookmark_records[:groonga] = groonga_id = @bookmarks.add
+    @uri_column[groonga_id] = "http://groonga.org/"
+    @comment_column[groonga_id] = "fulltext search engine"
+#     @content_column[groonga_id] = "<html>...</html>"
+
+    @bookmark_records[:cutter] = cutter_id = @bookmarks.add
+    @uri_column[cutter_id] = "http://cutter.souceforge.net/"
+    @comment_column[cutter_id] = "a unit testing framework for C"
+#     @content_column[cutter_id] = "<html>xxx</html>"
+  end
+
+  def setup_class
+    @bookmark_class = Class.new(ActiveGroonga::Base)
+  end
+
+  def teardown_sand_box
+    teardown_tmp_directory
+  end
+
   def teardown_tmp_directory
     FileUtils.rm_rf(@tmp_dir.to_s)
   end
+
 end
