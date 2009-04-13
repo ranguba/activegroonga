fc02214ca8523a4de76c304e327d82ab32ffaebe
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-04-13 07:43:56 +0000

    * add scaffolded user and post.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/activegroonga/trunk@81 2d713de4-4fbb-4d10-8406-bb89ef438322

--- examples/blog/config/routes.rb
+++ examples/blog/config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :posts
+
+  map.resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
