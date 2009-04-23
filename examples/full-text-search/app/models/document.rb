104bd6044f4510c085a35613dbb9eca7254aa642
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-04-23 07:09:52 +0000

    * move to examples path.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/activegroonga/trunk@152 2d713de4-4fbb-4d10-8406-bb89ef438322

--- examples/full-text-search/app/models/document.rb
+++
@@ -1,2 +0,0 @@
-class Document < ActiveGroonga::Base
-end
--- examples/full-text-search/test/unit/source_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SourceTest < ActiveSupport::TestCase
-  test "find by name" do
-    assert_equal(sources(:wikipedia_ja), Source.find_by_name("Wikipedia (ja)"))
-  end
-end
