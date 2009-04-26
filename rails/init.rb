f41e71fe4d536b7128590390af89e569018da03a
kou <kou@2d713de4-4fbb-4d10-8406-bb89ef438322>
2009-04-26 12:19:29 +0000

    * support logging.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/groonga/activegroonga/trunk@175 2d713de4-4fbb-4d10-8406-bb89ef438322

--- rails/init.rb
+++ rails/init.rb
@@ -2,6 +2,44 @@
 
 ActiveGroonga::Base.logger ||= Rails.logger
 
+case Rails.logger.level
+when ActiveSupport::BufferedLogger::DEBUG
+  log_level = :debug
+when ActiveSupport::BufferedLogger::INFO
+  log_level = :info
+when ActiveSupport::BufferedLogger::WARN
+  log_level = :warning
+when ActiveSupport::BufferedLogger::ERROR
+  log_level = :error
+when ActiveSupport::BufferedLogger::FATAL
+  log_level = :critical
+when ActiveSupport::BufferedLogger::UNKNOWN
+  log_level = :none
+else
+  log_level = :info
+end
+options = {:level => log_level}
+Groonga::Logger.register(options) do |level, time, title, message, location|
+  logger = ActiveGroonga::Base.logger
+  method_name = :info
+  case level
+  when :debug
+    method_name = :debug
+  when :info
+    method_name = :info
+  when :warning
+    method_name = :warn
+  when :error
+    method_name = :error
+  when :critical
+    method_name = :fatal
+  when :none
+    method_name = :unknown
+  end
+  message = "[#{title}] #{message}" unless title.blank?
+  logger.send(method_name, message)
+end
+
 configuration_file = config.groonga_configuration_file
 unless File.exist?(configuration_file)
   File.open(configuration_file, "w") do |file|
