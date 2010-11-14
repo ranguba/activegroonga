855fee19d853c038e2c65c6e402e367878e84971
Kouhei Sutou <kou@clear-code.com>
2010-11-14 20:58:10 +0900

    add ResultSet.

--- lib/active_groonga/result_set.rb
+++ lib/active_groonga/result_set.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2009-2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2010  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -13,37 +13,42 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-require 'pathname'
-
-require 'active_model'
-
-base_dir = Pathname(__FILE__).dirname
-rroonga_dir = (base_dir + "../../groonga").expand_path
-if rroonga_dir.exist?
-  $LOAD_PATH.unshift(rroonga_dir + "ext" + "groonga")
-  $LOAD_PATH.unshift(rroonga_dir + "lib")
-end
-
-require 'groonga'
-
 module ActiveGroonga
-  extend ActiveSupport::Autoload
+  class ResultSet
+    include Enumerable
 
-  eager_autoload do
-    autoload :VERSION
+    attr_reader :records
+    def initialize(records, klass)
+      @records = records
+      @klass = klass
+      @groups = {}
+    end
 
-    autoload :Error
+    def paginate(sort_keys, options={})
+      @records.paginate(sort_keys, options)
+    end
 
-    autoload :Base
-    autoload :Database
-    autoload :ResultSet
-    autoload :Schema
-    autoload :Persistence
-    autoload :Callbacks
+    def size
+      @records.size
+    end
 
-    autoload :Migrator
-    autoload :Migration
+    def expression
+      @records.expression
+    end
+
+    def group(key)
+      @groups[key] ||= @records.group(key)
+    end
+
+    def each
+      @records.each do |record|
+        yield(instantiate(record.key))
+      end
+    end
+
+    private
+    def instantiate(record)
+      @klass.instantiate(record)
+    end
   end
 end
-
-I18n.load_path << (base_dir + 'active_groonga/locale/en.yml').to_s
