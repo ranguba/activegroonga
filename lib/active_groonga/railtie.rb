099353c997ddfa84a9f0d51966703d2b8937e31c
Kouhei Sutou <kou@clear-code.com>
2010-11-14 17:46:58 +0900

    require active_groonga.

--- lib/active_groonga/railtie.rb
+++ lib/active_groonga/railtie.rb
@@ -13,6 +13,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+require "active_groonga"
 require "rails"
 require "active_model/railtie"
 
