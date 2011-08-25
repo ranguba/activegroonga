a6f2e5e8d5b2c0b3c10c1706897767082753ba48
Kouhei Sutou <kou@clear-code.com>
2011-08-25 23:48:05 +0900

    use test-unit gem.

--- test/run-test.rb
+++ test/run-test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright (C) 2009-2010  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2009-2011  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -20,21 +20,12 @@
 require 'pathname'
 require 'shellwords'
 
+require "rubygems"
+gem "test-unit"
+require "test/unit"
+require "test/unit/notify"
+
 base_dir = Pathname(__FILE__).dirname.parent.expand_path
-test_unit_dir = base_dir + "test-unit"
-test_unit_notify_dir = base_dir + "test-unit-notify"
-test_unit_repository_base = "http://test-unit.rubyforge.org/svn"
-unless test_unit_dir.exist?
-  system("svn", "co", "#{test_unit_repository_base}/trunk",
-         test_unit_dir.to_s)
-end
-unless test_unit_notify_dir.exist?
-  test_unit_notify_repository = "#{test_unit_repository_base}/extensions/test-unit-notify/trunk/"
-  system("svn", "co",
-         test_unit_notify_repository.to_s,
-         test_unit_notify_dir.to_s) or exit(false)
-  system("svn", "up", test_unit_dir.to_s) or exit(false)
-end
 
 rroonga_dir = base_dir.parent + "rroonga"
 lib_dir = base_dir + "lib"
@@ -55,16 +46,9 @@ if rroonga_dir.exist?
   $LOAD_PATH.unshift(rroonga_dir + "lib")
 end
 
-$LOAD_PATH.unshift(test_unit_notify_dir + "lib")
-$LOAD_PATH.unshift(test_unit_dir + "lib")
-
 ENV["TEST_UNIT_MAX_DIFF_TARGET_STRING_SIZE"] = "10000"
 
-require 'test/unit'
-require 'test/unit/notify'
-
-ARGV.unshift("--priority-mode")
-ARGV.unshift("--notify")
+Test::Unit::Priority.enable
 
 $LOAD_PATH.unshift(lib_dir)
 
